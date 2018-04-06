8f298182a5d70ef3da064d84411d00a4231e7b90
Romain Warnan <romain.warnan@insee.fr>
2018-04-06 11:03:20 +0200

    All this whitespace

---
+++ src/test/java/fr/insee/regex/Lesson10Test.java
@@ -0,0 +1,47 @@
+package fr.insee.regex;
+
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.regex.Pattern;
+
+import org.junit.Test;
+
+/**
+ * All this whitespace  
+ * */
+public class Lesson10Test {
+
+	private static final String regex = ""; // TODO
+	
+	private static final Pattern pattern = Pattern.compile(regex);
+	
+	/*
+	 - match 	1. abc
+	 - match 	2.	 abc
+	 - match 	3. 		 abc
+	 - skip 	4.abc
+	*/
+	
+	@Test
+	public void regexShouldMatch_aaaabcc() {
+		assertThat("1. abc").matches(pattern);
+	}
+	
+	@Test
+	public void regexShouldMatch_aabbbbc() {
+		assertThat("2.	 abc").matches(pattern);
+	}
+	
+	@Test
+	public void regexShouldMatch_aacc() {
+		assertThat("3. 		 abc").matches(pattern);
+	}
+	
+	@Test
+	public void regexShouldNotMatch_a() {
+		assertThat("4.abc").doesNotMatch(pattern);
+	}
+}
+
+
