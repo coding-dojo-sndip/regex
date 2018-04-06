8ca7d50693110456cf13ea97c0595814c854ea96
Romain Warnan <romain.warnan@insee.fr>
2018-04-06 12:54:09 +0200

    It's all conditional

---
+++ src/test/java/fr/insee/regex/Lesson14Test.java
@@ -0,0 +1,47 @@
+package fr.insee.regex;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.regex.Pattern;
+
+import org.junit.Test;
+
+/**
+ * It's all conditional 
+ * */
+public class Lesson14Test {
+	
+	private static final String regex = ""; // TODO
+	
+	private static final Pattern pattern = Pattern.compile(regex);
+	
+	/*
+	 - match 	I love cats
+	 - match 	I love dogs
+	 - skip 	I love logs
+	 - skip 	I love cogs
+	*/
+	
+	@Test
+	public void regexShouldMatch_1() {
+		assertThat("I love cats").matches(pattern);
+	}
+	
+	@Test
+	public void regexShouldMatch_2() {
+		assertThat("I love dogs").matches(pattern);
+	}
+	
+	@Test
+	public void regexShouldSkip_1() {
+		assertThat("I love logs").doesNotMatch(pattern);
+	}
+	
+	@Test
+	public void regexShouldSkip_2() {
+		assertThat("I love cogs").doesNotMatch(pattern);
+	}
+}
+
+
