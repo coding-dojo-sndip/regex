c0e2ad0cf8ed63fac2a6dbca07e6cd0964bc8730
Romain Warnan <romain.warnan@insee.fr>
2018-04-06 10:45:43 +0200

    Catching some zzz's

---
+++ src/test/java/fr/insee/regex/Lesson7Test.java
@@ -0,0 +1,35 @@
+package fr.insee.regex;
+
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.regex.Pattern;
+
+import org.junit.Test;
+
+/**
+ * Catching some zzz's 
+ * */
+public class Lesson7Test {
+
+	private static final String regex = ""; // TODO
+	
+	private static final Pattern pattern = Pattern.compile(regex);
+	
+	@Test
+	public void regexShouldMatch_wazzzzzup() {
+		assertThat("wazzzzzup").matches(pattern);
+	}
+	
+	@Test
+	public void regexShouldMatch_wazzzup() {
+		assertThat("wazzzup").matches(pattern);
+	}
+	
+	@Test
+	public void regexShouldNotMatch_aax() {
+		assertThat("wazup").doesNotMatch(pattern);
+	}
+}
+
+
