e49c9d1d458896114550a406644f4d9715148420
Romain Warnan <romain.warnan@insee.fr>
2018-04-06 10:42:22 +0200

    Character ranges

---
+++ src/test/java/fr/insee/regex/Lesson6Test.java
@@ -0,0 +1,50 @@
+package fr.insee.regex;
+
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.regex.Pattern;
+
+import org.junit.Test;
+
+/**
+ * Character ranges 
+ * */
+public class Lesson6Test {
+
+	private static final String regex = ""; // TODO
+	
+	private static final Pattern pattern = Pattern.compile(regex);
+	
+	@Test
+	public void regexShouldMatch_Ana() {
+		assertThat("Ana").matches(pattern);
+	}
+	
+	@Test
+	public void regexShouldMatch_Bob() {
+		assertThat("Bob").matches(pattern);
+	}
+	
+	@Test
+	public void regexShouldMatch_Cpc() {
+		assertThat("Cpc").matches(pattern);
+	}
+	
+	@Test
+	public void regexShouldNotMatch_aax() {
+		assertThat("aax").doesNotMatch(pattern);
+	}
+	
+	@Test
+	public void regexShouldNotMatch_bby() {
+		assertThat("bby").doesNotMatch(pattern);
+	}
+	
+	@Test
+	public void regexShouldNotMatch_ccz() {
+		assertThat("ccz").doesNotMatch(pattern);
+	}
+}
+
+
