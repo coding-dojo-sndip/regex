dbd2eabee277646a176d77fca59d83952335093c
Romain Warnan <romain.warnan@insee.fr>
2018-04-06 10:37:47 +0200

    Matching specific characters

---
+++ src/test/java/fr/insee/regex/Lesson4Test.java
@@ -0,0 +1,50 @@
+package fr.insee.regex;
+
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.regex.Pattern;
+
+import org.junit.Test;
+
+/**
+ * Matching specific characters 
+ * */
+public class Lesson4Test {
+
+	private static final String regex = ""; // TODO
+	
+	private static final Pattern pattern = Pattern.compile(regex);
+	
+	@Test
+	public void regexShouldMatch_can() {
+		assertThat("can").matches(pattern);
+	}
+	
+	@Test
+	public void regexShouldMatch_man() {
+		assertThat("man").matches(pattern);
+	}
+	
+	@Test
+	public void regexShouldMatch_fan() {
+		assertThat("fan").matches(pattern);
+	}
+	
+	@Test
+	public void regexShouldNotMatch_dan() {
+		assertThat("dan").doesNotMatch(pattern);
+	}
+	
+	@Test
+	public void regexShouldNotMatch_ran() {
+		assertThat("ran").doesNotMatch(pattern);
+	}
+	
+	@Test
+	public void regexShouldNotMatch_pan() {
+		assertThat("pan").doesNotMatch(pattern);
+	}
+}
+
+
