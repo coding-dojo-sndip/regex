e1029399a1d846f69cee758b1dc2b0fee7f17ace
Romain Warnan <romain.warnan@insee.fr>
2018-04-06 10:39:32 +0200

    Excluding specific characters

---
+++ src/test/java/fr/insee/regex/Lesson5Test.java
@@ -0,0 +1,35 @@
+package fr.insee.regex;
+
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.regex.Pattern;
+
+import org.junit.Test;
+
+/**
+ * Excluding specific characters
+ * */
+public class Lesson5Test {
+
+	private static final String regex = ""; // TODO
+	
+	private static final Pattern pattern = Pattern.compile(regex);
+	
+	@Test
+	public void regexShouldMatch_hog() {
+		assertThat("hog").matches(pattern);
+	}
+	
+	@Test
+	public void regexShouldMatch_dog() {
+		assertThat("dog").matches(pattern);
+	}
+	
+	@Test
+	public void regexShouldNotMatch_bog() {
+		assertThat("bog").doesNotMatch(pattern);
+	}
+}
+
+
