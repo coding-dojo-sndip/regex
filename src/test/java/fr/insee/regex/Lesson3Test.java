eb738ff478de56b51216121d5300013454746ce8
Romain Warnan <romain.warnan@insee.fr>
2018-04-06 10:28:20 +0200

    Le point

---
+++ src/test/java/fr/insee/regex/Lesson3Test.java
@@ -0,0 +1,38 @@
+package fr.insee.regex;
+
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.regex.Pattern;
+
+import org.junit.Test;
+
+/**
+ * The Dot
+ * */
+public class Lesson3Test {
+
+	private static final String regex = ""; // TODO
+	
+	private static final Pattern pattern = Pattern.compile(regex);
+	
+	@Test
+	public void regexShouldMatch_cat() {
+		assertThat("cat.").matches(pattern);
+	}
+	
+	@Test
+	public void regexShouldMatch_896() {
+		assertThat("896.").matches(pattern);
+	}
+	
+	@Test
+	public void regexShouldMatch_signs() {
+		assertThat("?=+.").matches(pattern);
+	}
+	
+	@Test
+	public void regexShouldNotMatch_abc1() {
+		assertThat("abc1").doesNotMatch(pattern);
+	}
+}
